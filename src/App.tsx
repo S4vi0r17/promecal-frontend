855f8148f24a2ce0b82fe855badcf743b90095c5
S4vi0r <gbp17@outlook.com>
2024-11-05 16:48:00 -0500

    implement routing with react-router-dom and add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import Login from './pages/Login'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Dash from './pages/Dash/Dash';
+import Ejemplo1 from './pages/Dash/ejemplo1/Ejemplo1';
+import Ejemplo2 from './pages/Dash/ejemplo2/Ejemplo2';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
-    <Login />
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Dash />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate to="1" replace />} />
+          <Route path="1" element={<Ejemplo1 />} />
+          <Route path="2" element={<Ejemplo2 />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
