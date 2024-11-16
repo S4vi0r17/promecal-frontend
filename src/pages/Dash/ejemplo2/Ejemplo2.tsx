b4a7ac6b850489a44387717f461e37413d82c9e9
Jefferson Asencios <jefferson30.ao@gmail.com>
2024-11-16 00:57:43 -0500

    ga

--- src/pages/Dash/ejemplo2/Ejemplo2.tsx
+++ src/pages/Dash/ejemplo2/Ejemplo2.tsx
@@ -12,6 +12,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import api from '../../../services/api';
 
+
 export default function GestionOrdenesDeTrabajo() {
   const [ordenes, setOrdenes] = useState<OrdenDeTrabajo[]>([]);
   const [loading, setLoading] = useState(true);
