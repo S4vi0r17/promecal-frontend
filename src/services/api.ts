90ca236e1640c17a5a374fc90daa3ec5e17ae8bf
S4vi0r <gbp17@outlook.com>
2024-12-10 00:47:48 -0500

    change envionment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://promecal-backend.onrender.com/',
 });
 
 api.interceptors.request.use(
